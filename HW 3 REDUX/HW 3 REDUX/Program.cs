4f45a0c70ee5cf9f668f5ca2028e2608f16160ab
Brianteruya <bnt7728@rit.edu>
2026-02-16 09:18:09 -0500

    HW addition

---
+++ HW 3 REDUX/HW 3 REDUX/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HW_3_REDUX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

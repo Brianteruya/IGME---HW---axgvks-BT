7a4aa849cf955b44e77f515ceef0bdbb908e9ee4
Brianteruya <bnt7728@rit.edu>
2026-03-16 22:46:14 -0400

    Add files via upload

--- monopoly framework/monopoly framework/Program.cs
+++ monopoly framework/monopoly framework/Program.cs
@@ -10,16 +10,19 @@
             electric.SpaceInfo("bob");
 
             //==============================
-            //HW 3 RETRY (Arch. for main)
+            //HW 5 
             //==============================
             /*
-             * Classes needed (properties and variables needed)
-             * main
-             *  - Console.WriteLine()/.ReadLine()
-             *      these will ask the user questions
-             *      and print out game info during runtime
-             *  - Playername      
-             * 
+             * Project: Architecture
+             * Developer: Brian Teruya
+             * Purpose: do some bullshit i'unno anymore
+             * last modified: 2/23/2026
+             * last modification: Look I'm not going to take this class seriously because I'm literally being hoed into staying in this smelly ass class
+             * I came into RIT hoping I can do some practical and experimental shit and all I got was this bull shit
+             * which isn't even going to matter if I keep going down this path, where either:
+             * 1: I clutch up this and 106, but will eventually have to throw all this useless ass knowledge away for some random ass game engine where I have to reinvent gravity
+             * 2: I fail the fuckin course and get kicked out of this fraudulent ass school
+             * so in the most respectful way I can be in my current situation, fuck this class and fuck you.
              * 
              */
         }
